3c4ec49d9841b20875d216c3ea8bded9bd7fd50b
subract <self@subract.dev>
2023-09-06 16:00:15 -0500

    🔧 Use viper for configuration

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"os/signal"
@@ -9,23 +8,12 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/op/go-logging"
+	"github.com/spf13/viper"
 	"github.com/subract/hetzplay/hetzner"
 )
 
 // TODO: figure out the proper way to do versioning
 var version string = "0.1.0"
-
-// Bot parameters
-// TODO: Migrate to env vars/conf file
-var (
-	GuildID         = flag.String("guild", "", "Guild ID. If not passed - bot registers commands globally")
-	BotToken        = flag.String("discord_token", "", "Discord bot access token")
-	HetznerToken    = flag.String("hetzner_token", "", "Hetzner API token")
-	ServerName      = flag.String("server", "", "Hetzner server name. Must be unique within project.")
-	BackupSnapCount = flag.Uint("backup_snaps", 1, "How many backup snapshots to keep")
-	LogLevel        = flag.String("log", "notice", "One of [critical error warning notice info debug]")
-)
-
 var serverMgr *hetzner.ServerManager
 var session *discordgo.Session
 var log = logging.MustGetLogger("log")
@@ -50,18 +38,33 @@ var commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.Interac
 
 func init() {
 
-	// TODO: Validate args
-	flag.Parse()
+	// Handle configuration
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+	viper.SetEnvPrefix("hetzplay")
+	viper.BindEnv("log_level")
+
+	viper.SetDefault("backup_snaps", 1)
+	viper.SetDefault("log_level", "notice")
+
+	err := viper.ReadInConfig()
+	if err != nil { // config
+		log.Error(err)
+		os.Exit(1)
+	}
 
 	// Set up logging
-	err := initializeLogging(*LogLevel)
+	err = initializeLogging(viper.GetString("log_level"))
 	if err != nil { // bad log level
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
+	log.Notice("Starting Hetzplay", version)
+
 	log.Debug("Initializing Discord bot")
-	session, err = discordgo.New("Bot " + *BotToken)
+
+	session, err = discordgo.New("Bot " + viper.GetString(("discord.token")))
 	if err != nil {
 		log.Fatalf("Invalid bot parameters: %v", err)
 	}
@@ -79,10 +82,16 @@ func init() {
 	})
 
 	// Create server manager
-	serverMgr, err = hetzner.NewServerManager(*ServerName, int(*BackupSnapCount), *HetznerToken, version, log)
+	serverMgr, err = hetzner.NewServerManager(viper.GetString("hetzner.server.name"),
+		viper.GetInt("hetzner.server.backup_snaps"),
+		viper.GetString("hetzner.token"),
+		version,
+		log)
+
 	if err != nil {
 		log.Fatalf("failed to initialize server manager: %s", err)
 	}
+	log.Notice("Logged in to Hetzner")
 
 }
 
@@ -95,7 +104,7 @@ func main() {
 
 	log.Debug("Adding commands to bot session.")
 	for _, v := range commands {
-		_, err := session.ApplicationCommandCreate(session.State.User.ID, *GuildID, v)
+		_, err := session.ApplicationCommandCreate(session.State.User.ID, viper.GetString("discord.guild_id"), v)
 		if err != nil {
 			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
 		}
@@ -115,13 +124,13 @@ func main() {
 	log.Notice("Shutting down.")
 	log.Debug("Removing registered bot commands")
 
-	registeredCommands, err := session.ApplicationCommands(session.State.User.ID, *GuildID)
+	registeredCommands, err := session.ApplicationCommands(session.State.User.ID, viper.GetString("discord.guild_id"))
 	if err != nil {
 		log.Fatalf("Could not fetch registered commands: %v", err)
 	}
 
 	for _, v := range registeredCommands {
-		err := session.ApplicationCommandDelete(session.State.User.ID, *GuildID, v.ID)
+		err := session.ApplicationCommandDelete(session.State.User.ID, viper.GetString("discord.guild_id"), v.ID)
 		if err != nil {
 			log.Fatalf("Cannot delete '%v' command: %v", v.Name, err)
 		}
